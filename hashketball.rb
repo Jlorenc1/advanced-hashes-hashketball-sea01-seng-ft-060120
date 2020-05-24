e63243b726c18ec8390241e260cf91c47327f543
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-24 17:50:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,10 +127,10 @@ def game_hash
   }
 end
 
-def num_points_scored(player_search)
+def num_points_scored(name)
   game_hash.each do | team, team_info|
     team_info[:players].each do | player|
-      if player[:player_name] == player_search
+      if player[:player_name] == name
         return player[:points]
       end
     end
