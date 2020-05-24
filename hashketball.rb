6a54cf6d2d951412859e70a944619b285e9acf22
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-24 17:51:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -138,8 +138,8 @@ def num_points_scored(name)
 end
 
 def shoe_size(name)
-  game_hash.each do | team, team_info|
-    team_info[:players].each do | player|
+  game_hash.each do |team, stats|
+    stats[:players].each do |player|
       if player[:player_name] == name
         return player[:shoe]
       end
